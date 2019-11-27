108f74f938265ddce1ad898354593693fe3dfb56
1631401 <1631401@stud.hs-mannheim.de>
2019-11-27 19:45:27 +0100

    Standardpfad geändert

--- SignalRMvcChat/SignalRMvcChat/App_Start/RouteConfig.cs
+++ SignalRMvcChat/SignalRMvcChat/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SignalRMvcChat
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "chat", id = UrlParameter.Optional }
             );
         }
     }
